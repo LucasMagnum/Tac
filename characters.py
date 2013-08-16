f92026e6127c4fad33fc50578f021a48fdfbb17f
Lucas Magnum <lucasmagnum2@gmail.com>
2013-08-15 23:37:58 -0300

    movimentando a nave pela tela. ainda sem restrições.

--- characters.py
+++ characters.py
@@ -2,12 +2,48 @@
 
 import pygame
 
+from collections import namedtuple
+
 
 class SpaceCraft(object):
 	def __init__(self, size):
-		self.size = size
-		self.img = pygame.image.load('resources/spacecraft_01.png')
-		self.position = (100, 100)
+		self.width, self.height = size
+		self.spacecraft_name = 'spacecraft_01'
+		self.img = self.spacecraft_name
+		self.position = [100, 100]
+		import ipdb; ipdb.set_trace()
+
+	def set_position(self, x=0, y=0):
+		""" 
+			Definir a nova posição da nave 
+			Para mover para direita, aumente no X
+			Para mover para esquerda, diminua no X
+			Para mover para cima, aumente o Y
+			Para mover para baixo, diminua o Y
+		"""
+		new_position_x = self.position[0] + x
+		new_position_y = self.position[1] + y
+
+		if new_position_x > 0 and new_position_x < self.width:
+			self.position[0] = new_position_x
+
+		if new_position_y > 0:
+			self.position[1] = new_position_y
+
+		if y < 0:
+			self.turbe_mode(active=True)
+		else:
+			self.turbe_mode(active=False)
+
+	def turbe_mode(self, active=False):
+		""" Ligar a turbina da nave """
+		self.img = '%s_active' % self.spacecraft_name if active else self.spacecraft_name
+
+	def load_img(self):
+		""" Carregar imagem da nave """
+		self.img_load = pygame.image.load('resources/%s.png' % self.img)
 
 	def draw(self):
-		return self.img, self.position
+		""" Retornar imagem e posição para ser desenhada """
+		self.load_img()
+		return self.img_load, self.position
--- main.py
+++ main.py
@@ -13,11 +13,12 @@ class SpaceInvaders(object):
         self.size = self.width, self.height = 1000, 600
 
     def load_itens(self):
-        # carregar images e arquivos utils
+        """ Carregar imagens, personagens e outros itens da tela """
         self.background = pygame.image.load('resources/space_background.jpg')
         self.spacecraft = SpaceCraft(size=self.size)
 
     def on_init(self):
+        """ Inicializar os modulos do pygame como outros detalhes do game """
         # inicializar modulos
         pygame.init()
         self.load_itens()
@@ -28,16 +29,32 @@ class SpaceInvaders(object):
         # setar o tamanho inicial da tela
         self._display = pygame.display.set_mode(self.size)
 
+    def on_keys(self):
+        """ Ações para as Keys pressionadas """
+        keys = pygame.key.get_pressed()
+
+        if keys[K_RIGHT]:
+            self.spacecraft.set_position(x=3)
+        if keys[K_LEFT]:
+            self.spacecraft.set_position(x=-3)
+        if keys[K_DOWN]:
+            self.spacecraft.set_position(y=3)
+        if keys[K_UP]:
+            self.spacecraft.set_position(y=-3)
+
     def on_event(self, event):
+        """ Ações para o eventos realizados no jogo """
         # se o evento for um quit, fechar
         if event.type == pygame.QUIT:
             self._running = False
 
     def on_loop(self):
+        """  Ações que devem ser disparadas a cada loop """
         # atualizar a pagina
         pygame.display.update()
         
     def on_render(self):
+        """ Itens que devem ser renderizados """
         # desenhar background na tela
         self._display.blit(self.background, (0, 0))
 
@@ -49,6 +66,7 @@ class SpaceInvaders(object):
         pygame.quit()
 
     def on_execute(self):
+        """ Responsável pela execução do Jogo """
         self.on_init()
 
         while(self._running):
@@ -57,6 +75,7 @@ class SpaceInvaders(object):
             for event in pygame.event.get():
                 self.on_event(event)
 
+            self.on_keys()
             self.on_loop()
 
         self.on_cleanup()
